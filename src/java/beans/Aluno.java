cc54788fcad5f3b8fee84442119a76e298e07e8f
Laercio Lima <laercio.lima@gmail.com>
2016-12-06 14:00:01 -0200

    Primeira versão do web service

---
+++ src/java/beans/Aluno.java
@@ -0,0 +1,104 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beans;
+
+import java.io.Serializable;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author laercio
+ */
+@XmlRootElement
+public class Aluno implements Serializable {
+
+    private int id;
+    private String nome;
+    private String telefone;
+    private String endereco;
+    private String site;
+    private String email;
+    private String foto;
+    private int nota;
+
+    public Aluno() {
+    }
+
+    public Aluno(int id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getSite() {
+        return site;
+    }
+
+    public void setSite(String site) {
+        this.site = site;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+
+    public int getNota() {
+        return nota;
+    }
+
+    public void setNota(int nota) {
+        this.nota = nota;
+    }
+
+    @Override
+    public String toString() {
+        return "Aluno{" + "id=" + id + ", nome=" + nome + ", telefone=" + telefone + ", endereco=" + endereco + ", site=" + site + ", email=" + email + ", foto=" + foto + ", nota=" + nota + '}';
+    }
+
+}
---
+++ src/java/webservices/AlunoResource.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package webservices;
+
+import beans.Aluno;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Produces;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PUT;
+
+/**
+ * REST Web Service
+ *
+ * @author laercio
+ */
+@Path("/aluno")
+public class AlunoResource {
+
+    @Context
+    private UriInfo context;
+    ArrayList<Aluno> alunos = new ArrayList<>();
+
+    /**
+     * Creates a new instance of CotacaoResource
+     */
+    public AlunoResource() {
+    }
+
+    /**
+     * Retrieves representation of an instance of webservices.AlunoResource
+     *
+     * @return an instance of java.lang.String
+     */
+   
+
+    @GET
+    @Path("/getAll")
+    @Produces("application/json")
+    public ArrayList<Aluno> getAlunos() {
+        try {
+            FileInputStream fis = new FileInputStream("alunos");
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            alunos = (ArrayList) ois.readObject();
+            ois.close();
+            fis.close();
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+
+        } catch (ClassNotFoundException c) {
+            System.out.println("Class not found");
+            c.printStackTrace();
+
+        }
+
+        return alunos;
+    }
+
+    /**
+     * PUT method for updating or creating an instance of AlunoResource
+     *
+     * @param content representation for the resource
+     * @return an HTTP response with content of the updated or created resource.
+     */
+    @POST
+    @Path("/saveAll")
+    @Consumes("application/json")
+    public void saveAll(ArrayList<Aluno> alunos) {
+        
+       
+
+        try {
+            FileOutputStream fos = new FileOutputStream("alunos");
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(alunos);
+            oos.close();
+            fos.close();
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }
+}
